6da1166b763c78e0b176f3121739df2c84b6ac58
Juan Luis Jimenez <jlja2@ensign.edu>
2023-10-27 22:56:31 -0600

    test-blog-fetch

---
+++ blog/blog.js
@@ -0,0 +1,42 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+ // Fetch and display posts with their comments on page load
+ fetchPostsAndComments();
+     
+});
+
+// Function to fetch and display posts with their comments
+function fetchPostsAndComments() {
+    fetch('http://146.190.137.89/posts', {
+      "method": "GET",
+      headers: {'Content-type':'application/json'}
+  }) 
+      .then((response) => response.json())
+      .then((data) => {
+        // You can process the data to group posts with their comments
+        // and display them in your HTML, for example:
+        let postsContainer = document.querySelector('.post-container');
+        data.data.map((post) => {
+          const postDiv = document.createElement('div');
+          postDiv.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.content}</p>
+            <h3>Comments:</h3>
+          `;
+          
+
+            const commentDiv = document.createElement('div');
+            commentDiv.innerHTML = `
+              <strong>${post.comment}:</strong> ${post.comment}
+            `;
+            postDiv.appendChild(commentDiv);
+
+          
+          postsContainer.appendChild(postDiv);
+        });
+      });
+  }
+  
+ 
