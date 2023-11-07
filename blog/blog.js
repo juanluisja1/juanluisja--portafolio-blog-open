87aebd4b7a5d1d5fc4ffb3a2d646142a40fc2a18
Juan Luis Jimenez <jlja2@ensign.edu>
2023-11-07 02:00:51 -0700

    add-comment--after-posted-sucessfully

--- blog/blog.js
+++ blog/blog.js
@@ -1,13 +1,9 @@
 
-
 document.addEventListener("DOMContentLoaded", function () {
 
  // Fetch and display posts with their comments on page load
  fetchPostsAndComments()
 });
-
-
-
 
 /**
  * 
@@ -17,12 +13,155 @@ document.addEventListener("DOMContentLoaded", function () {
  * number is what matter the most to indentify element 
  */
 
-function comment__post(eventTarget){
+// API /post route client
+
+async function comment__post(eventTarget){
+
+  console.log('comment__post'+eventTarget.target.id);
+
+  let id = eventTarget.target.id.split(":")[1];
+  let container__comment_form = document.getElementById(id)
+  let comment__txt = document.getElementById("button_id:"+id);
+
+  let comment_user = document.getElementById("username-input+"+id);
+  let comment_textarea = document.getElementById("comment-input:"+id);
+
+  let comment_data = {
+    "status": true,
+    "comment": comment_textarea.value,
+    "post_id": id,
+    "username": comment_user.value,
+    "author_id": 3
+  }
+  // api/resource?param1=value1&param2=value2
+  // Make a GET request to the /get_user_id route
+  let validadatedUser = await fetch(`http://localhost:3000/get_user_id?username=${comment_user.value}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message === "User found successfully") {
+        const userId = data.data.user_id;
+        comment_data.author_id = userId;
+        console.log(`User found with ID: ${userId}`);
+        return comment_data;
+      } else if (data.message === "User not found, jhon_doe") {
+        console.log(" 57 User not found, jhon_doe");
+      }
+      else{
+        comment_data.status = false;
+      }
+    })
+    .catch(() => {
+      console.error("user john_doe");
+      comment_data.author_id = 3;
+      comment_data.username = "john_doe";
+      console.log(comment_data);
+    });
+
+    // id INTEGER PRIMARY KEY,
+    // comment TEXT NOT NULL,
+    // post_id INTEGER,
+    // author_id INTEGER,
+  
+  console.log(`this is the validated user:${validadatedUser}` );
+
+  post_comment_post(comment_data);
+
+  //update post comment if success comment_data.status == true
+
+  if(comment_data.status) {
+    let comment_list = document.getElementById("comment-list:"+id);
+
+    //creating all things control the comment
+
+  //   let comment_data: {
+  //     status: boolean;
+  //     comment: any;
+  //     post_id: any;
+  //     username: any;
+  //     author_id: number;
+  // }
+
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+    const commentItem = document.createElement('div');
+    commentItem.classList.add('comment');
+    commentItem.id = comment_data.username+':'+comment_data.post_id;
+    commentItem.textContent = comment_data.comment;
+    
+    const comment_author= document.createElement('div');
+    comment_author.classList.add('comment_author');
+    comment_author.textContent =` by ${comment_data.username} @ ${year}-${month}-${day}`;
+
+    //action to reply
+    const div_reply_button= document.createElement('div');
+    const button = document.createElement('button');
+    button.classList.add('button_comment_reply');
+    button.addEventListener('click', function (event) {
+      reply_comment(event);
+    });
+    button.innerText = `Reply to ${comment_data.username}`;
+    button.id = `${comment_data.username}:${comment_data.post_id}`
+    div_reply_button.appendChild(button);
+
+    //add all elements to 
+    comment_author.appendChild(div_reply_button);
+    commentItem.appendChild(comment_author);
+    comment_list.appendChild(commentItem);
+
+  }
+
+  console.log(comment_textarea.value);
+  comment_textarea.value = "";
+  // comment_textarea.textContent ='';
+
+  container__comment_form.classList.add("hide");
+
+  comment__txt.classList.remove('hide');
 
   console.log(eventTarget.target.id);
-  
+
+
+
 
 }
+
+/**
+ * 
+ * @param {comment,post_id, author_id} postData 
+ */
+
+function post_comment_post(postData){
+
+  fetch('http://localhost:3000/post_comment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      //body: JSON.stringify(postData),
+      body: JSON.stringify( {"comment": postData.comment, "post_id": postData.post_id, "author_id": postData.author_id})
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response from the server
+        console.log(data.message); // You can display a success message to the user
+       // postForm.reset(); // Clear the form
+      })
+      .catch((error) => {
+        console.error('Error posting data: ', error);
+        // Handle the error, e.g., display an error message to the user
+      });
+  }
+
+
 
 function cancel__comment (eventTarget){
   let id = eventTarget.target.id.split(":")[1];
@@ -52,7 +191,7 @@ function reply_comment(eventTarget){
 
 
   console.log(comment_textarea.value);
-  comment_textarea.value = "";
+  comment_textarea.value = '@'+eventTarget.target.id.split(":")[0];
   // comment_textarea.textContent ='';
 
   container__comment_form.classList.remove("hide");
@@ -60,7 +199,6 @@ function reply_comment(eventTarget){
   comment__txt.classList.add('hide');
 
   console.log(eventTarget.target.id);
-
 
   console.log('reply_comment '+eventTarget.target.id)
 
@@ -103,6 +241,7 @@ function actionReply (eventTarget){
 
 
 
+
 //  document.querySelectorAll('button_comment_reply').addEventListener( 'click', function ( event ) {
  
 //   let form_id = event.target.id;
@@ -137,6 +276,7 @@ function fetchPostsAndComments() {
 
           const commentList = document.createElement('ul');
           commentList.className = 'comment-list';
+          commentList.id = "comment-list:"+post.post_id;
 
           post.comments.forEach((comment) => {
             const commentItem = document.createElement('div');
@@ -199,8 +339,8 @@ function fetchPostsAndComments() {
         commentForm.classList.add('comment-form');
         // commentForm.id = post.post_id
         commentForm.innerHTML = `
-          <input type="email" id="username-input+${post.post_id}" name="username" rows="4" cols="2" placeholder="email/username">
-          <textarea id="comment-input:${post.post_id}" class="comment_input" name="${post.post_id}" placeholder="Comments" rows="4" cols="2"></textarea>
+          <input type="email" id="username-input+${post.post_id}" name="username" rows="4" cols="2" placeholder="email/username" required>
+          <textarea id="comment-input:${post.post_id}" class="comment_input" name="${post.post_id}" placeholder="Comments" rows="4" cols="2" required></textarea>
         `;
         
        const container_submit_button =document.createElement('div');
